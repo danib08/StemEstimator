a26ab00c718ed745716afcb5ae906284db7a3f5d
Daniela Brenes <danibreo6@gmail.com>
2024-05-19 11:57:08 -0600

    [fix]: change wrong function calling name

--- stemestimator/utils.py
+++ stemestimator/utils.py
@@ -34,7 +34,7 @@ def open_3d_paint(nppoints, color_map='jet', reduce_for_vis=False, voxel_size=0.
         visualizer = open3d.visualization.Visualizer()
         visualizer.create_window()
         options = visualizer.get_render_option()
-        options.background_color = np.asarray([0, 0, 0])
+        options.background_color = np.asarray([1, 1, 1])
         options.point_size = pointsize
 
         if len(nppoints) > 1:
@@ -216,7 +216,6 @@ def rotation_matrix_from_vectors(vector1, vector2):
     matrix = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
     rotation_matrix = np.eye(3) + matrix + matrix.dot(matrix) * ((1 - c) / (s ** 2))
     return rotation_matrix
-
 
 def get_stem_sections(points, num_sections=10):
     """Divides the stem point cloud into sections.
